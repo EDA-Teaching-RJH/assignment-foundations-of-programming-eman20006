d165d8ff347f1ff46f17f735aa047979e40b3049
eman20006 <ef392@kent.ac.uk>
2026-02-10 18:58:56 +0000

    Started fleet_manager coding

---
+++ fleet_manager.py
@@ -0,0 +1,10 @@
+
+n = ["Crusher", "Troi", "Picard", "LaForge", "Data"]
+r = ["Commander", "Lieutenant", "Captain", "Lieutenant", "Lieutenant"]
+d = ["Sciences", "Sciences", "Command", "Operations", "Operations"]
+i = ["", "", "SP-937-215", "", ""]
+
+def initial_database:
+
+for i in range(len(n)):
+                print(n[i] + " - " + r[i] + " - " + d[i] + " - " + i[i]) 

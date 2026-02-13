3740b81b7fd92a5a29b7763f7d56ffb91a7d9e12
eman20006 <ef392@kent.ac.uk>
2026-02-13 13:28:05 +0000

    1. made lists and created database function

--- fleet_manager.py
+++ fleet_manager.py
@@ -2,16 +2,39 @@
 n = ["Crusher", "Troi", "Picard", "LaForge", "Data"]
 r = ["Commander", "Lieutenant", "Captain", "Lieutenant", "Lieutenant"]
 d = ["Sciences", "Sciences", "Command", "Operations", "Operations"]
-i = ["", "", "", "", ""]
+i = ["BC87", "DT87", "JP87", "GF87", "LD87"]
 
-def main():
-
-
-
+def fleet_manager():
+    print("--Booting database menu--")
     def initial_database():
-    
-        for i in range(len(n)):
-            print(n[i] + " - " + r[i] + " - " + d[i] + " - " + i[i]) 
-            return 
+        opt = input("Would you like to view current database? ")
+        if opt == "yes":
+            for x in range(len(n)):
+                print(n[x] + " - " + r[x] + " - " + d[x] + " - " + i[x]) 
+        elif opt == "no":
+            print("...")
+        else:
+            print("Invalid input")
+            initial_database()
 
+    initial_database()
+
+    def display_menu():
+        opt = input("Would you like to view display menu? ")
+        if opt == "yes": 
+            print("What is your full name? ")
+            print("Here are the current options: ")
+            print(n)  
+            name = input("Input selection: ")
+            
+        elif opt == "no":
+            print("...")
+        else:
+                print("Invalid input")
+                display_menu()
+        
+    display_menu()
+
+        
+fleet_manager()
      

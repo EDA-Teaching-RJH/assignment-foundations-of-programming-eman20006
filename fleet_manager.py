8e9e04963c2be8a3f277b635bafb350e1c7e03c1
eman20006 <ef392@kent.ac.uk>
2026-02-15 03:33:04 +0000

    progress on change rank

--- fleet_manager.py
+++ fleet_manager.py
@@ -7,78 +7,103 @@ print("--Booting database menu--")
 print("--Syncing data--")
 
 def main():
+    opt = input("Would you like to view display menu? ")
+    if opt == "yes": 
+        print("What is your full name? ")
+        name = input("Input: ")
+        if name in (n):
+            print("Welcome: " + name + " you are currently logged in.")
+        else:
+            print("Invalid name.")
+            
+    elif opt == "no":
+                print("Shutting down...")
+                return
+    else:
+        print("Invalid input")
+        main()
 
     def display_menu():
-        opt = input("Would you like to view display menu? ")
-        if opt == "yes": 
-            print("What is your full name? ")
-            name = input("Input: ")
-            if name in (n):
-                print("Welcome: " + name + "you are currently logged in.")
-                print("What would you like to do: ")
-                print("1.Add crew member: ")
-                print("2.Remove crew member: ")
-                print("3.Update rank: ")
-                print("4.Display roster: ")
-                print("5.Search crew member: ")
-                print("6.Filter by division: ")
-                print("7.Calculate payroll: ")
-                print("8.Count officers: ")
+        
+            print("--Menu--")
+            print("1.Add crew member: ")
+            print("2.Remove crew member: ")
+            print("3.Update rank: ")
+            print("4.Display roster: ")
+            print("5.Search crew member: ")
+            print("6.Filter by division: ")
+            print("7.Calculate payroll: ")
+            print("8.Count officers: ")
+            print("9.Shutdown: ")
                 
-                
+            opt = input("Selection: ")
+            if opt == "1":
+                add_member()
+            if opt == "2":
+                remove_member()
+            if opt == "3":
+                update_rank()
+            if opt == "4":
+                display_roster()
+            if opt == "5":
+                search_crew_member()
+            if opt == "6":
+                filter_by_division()
+            if opt == "7":
+                calculate_payroll()
+            if opt == "8":
+                calculate_officers()
+            if opt == "":
+                shutdown()
             else:
-                print("Invalid name.")
-                display_menu()
-
-
-        elif opt == "no":
-            print("Shutting down...")
-            
-        else:
                 print("Invalid input")
-                display_menu()
-        add_member()
-        remove_member()
+            display_menu()
 
     def add_member():
-        opt = input("Select option:")
-        if opt == "1":
-            new_name = input("Name: ")
-            new_rank = input("Rank: ")
-            new_div = input("Division: ")
-            new_id = input("Id: ")
+        print("Input to add crew member:")
+        new_name = input("Name: ")
+        new_rank = input("Rank: ")
+        new_div = input("Division: ")
+        new_id = input("Id: ")
+        if new_id in (i):
+            print("Invalid, ID already in use.")
+            display_menu()
+        else:  
             n.append(new_name)
             r.append(new_rank)
             d.append(new_div)  
             i.append(new_id)
-            if id in (i):
-                print("Invalid, ID already in use.")
-                display_menu()
-            else:    
-                print("Crew member added.")
-         
+            print("Crew member added.")
+            print(d)
+            return
     def remove_member():
-        opt = input("Select option:")
-        if opt == "2":
-            rem = input("ID to remove: ")
-            if rem not in i:
-                print("ID is not in database.")
-            else:
-             idx = i.index(rem)
-             n.pop(idx)
-             r.pop(idx)
-             d.pop(idx)
-             i.pop(idx)
-             print("Crew member removed.")
         
-            remove_member()  
+        rem = input("ID to remove: ")
+        if rem not in i:
+            print("ID is not in database.")
+        else:
+            idx = i.index(rem)
+            n.pop(idx)
+            r.pop(idx)
+            d.pop(idx)
+            i.pop(idx)
+            print("Crew member removed.")
+             
 
     def update_rank():
-        opt = input("Select option:")
-        if opt == "3":
-         updt = input("Input crew ID to update: ")
-         
-         update_rank()
+        
+        updt = input("Input crew ID to update: ")
+        if updt in i:
+            i.index(updt) 
+            crnk = input("What is the new rank? ")
+            r[i.index(updt)] = crnk
+            print("Rank updated.")
+            for x in range(len(n)): 
+                print(n[x] + " - " + r[x] + " - " + d[x] + " - " + i[x])                #insert rank at list number i want to change
+        else:
+            print("Invalid ID.")                                 #then remove said list number by pop but of + 1
+
+            
     def display_roster():
         if opt == "4":
          display_roster()
@@ -93,7 +118,14 @@ def main():
          calculate_payroll()
     def calculate_officers():
         if opt == "8":
-            calculate_officers()    
+            calculate_officers()
+    def shutdown():
+        if opt == "9":
+            print("Shutting down...")
+            exit()
+            shutdown()
+            
+                
 
     display_menu()  
 main()
